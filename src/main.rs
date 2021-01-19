18f56c29765b55dca77d865f6f533db1861a2970
WiZ3910 <mikuto.matsuo@icloud.com>
2021-01-19 19:40:55 +0900

    - 非同期の文法とよく使われるクレートを学んだ - トレイトを学び、マクロによって、mainをasync化したり、traitにasync関数を追加したりと言語を拡張出来ることを学んだ。

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use core::future::Future;
+
 
 
 fn main() {
@@ -310,8 +312,270 @@ fn main() {
     //NaNが存在すると、Unwrap時にpanicが起こる
     x.sort_by(|a,b| a.partial_cmp(b).unwrap());
     println!("{:?}", x);// [0.1, 0.2, 0.3, 0.4, 0.2]
-
+    //partialさえ実装していれば、普通に、演算子を使うことも出来るようになる。
     println!("{:?}", 1.0 < 1.1);
+
+    /*ゼロコスト抽象化*/
+    trait Tweet {
+        fn tweet(&self);
+        //デフォルト実装も追加することが出来るのか。
+        fn tweet_twice(&self) {
+            self.tweet();
+            self.tweet();
+        }
+        fn shout(&self) {
+            //これは笑った
+            println!("Uooooooooooohhh!!!");
+        }
+    }
+    struct Dove; //鳩
+    struct Duck; //アヒル
+    impl Tweet for Dove {
+        fn tweet(&self) { println!("Coo!"); }
+    }
+    impl Tweet for Duck { 
+        fn tweet(&self) { println!("Quack!");}
+    }
+
+    let dove = Dove;
+    dove.tweet();
+    dove.tweet_twice();
+    dove.shout();
+    let duck = Duck;
+    //動的ディスパッチ
+    let bird_vec: Vec<Box<dyn Tweet>> = vec![Box::new(dove), Box::new(duck)];
+    for bird in bird_vec {
+        bird.tweet();
+    }
+    //マーカートレイトについて
+    /*
+    - Copy : 値の所有権を渡す代わりに、値のコピーを行うようにする(所有権は後ほど)
+    - Send : スレッド境界を超えて、所有権を転送できることを示す
+    - Sized: メモリ上でサイズが決まっていることを示す
+    - Sync : スレッド間で安全に参照を共有できることを示す
+    */
+
+    //ジェネリクス
+    fn make_tuple<T,S>(t: T, s:S) -> (T,S) {
+        (t,s)
+    }
+    let tp = ("I'm", Emotion::Happy);
+
+    let mut important_data = "Hello World!".to_string();
+    important_data = calc_data(important_data); //値の所有権を渡し、返してもらう。
+    calc_data2(&important_data); // まあ、参照渡しだね。これを借用というらしい。
+
+
+
+    fn calc_data(data: String) -> String {
+        println!("{}",data);
+        data
+    }
+    fn calc_data2(data: &String)  {
+        println!("{}",data);
+    }
+{
+    let x = 5;
+    let y = &x; //１回目の不変な参照渡し
+    let z = &x; //2回目の``
+    dbg!(x);
+    dbg!(y);
+    dbg!(z);
+}
+{
+    let mut x = 5;
+    let y = &mut x;
+    //let z = &mut x; //２回目の可変な参照渡し（エラー）
+    dbg!(y);
+    //dbg!(z);
+}
+{
+    let mut x = 5;
+    let y = &x;
+    //let z = &mut x; //可変な参照渡し（これもエラー）
+    dbg!(y);
+    //dbg!(z);
+}
+    let y : i32 = i32::default();
+    {
+        let x = 5;
+        //y = &x; // <- x does not live long enough.
+        dbg!(x);
+        //xのライフタイム終了
+    }
+    dbg!(y);
+    //xよりyが長生きすることはできない。
+
+    //スレッドと所有権の移動
+    use std::thread;
+    let mut handles = Vec::new();
+    for i in 0..10 {
+        handles.push(thread::spawn(move || {
+            println!("workder thread. : {}",i);
+        }));
+    }
+    for handle in handles {
+        let _ = handle.join();
+    }
+    
+    use std::sync::{Arc,Mutex};
+    //スレッド間の情報共有
+    let mut handles = Vec::new();
+    let data = Arc::new(Mutex::new(vec![1; 10]));
+    for x in 0..10 {
+        let data_ref = data.clone();
+        handles.push(std::thread::spawn(move || {
+            let mut data = data_ref.lock().unwrap();
+            data[x] += 1;
+        }));
+    }
+
+    for handle in handles {
+        let _ = handle.join();
+    }
+    dbg!(data);
+    
+    //メッセージパッシング方式で実装してみる
+    use std::sync::mpsc;
+    let mut handles = Vec::new();
+    let mut data = vec![1; 10];
+    let mut snd_channels = Vec::new();
+    let mut rcv_channels = Vec::new();
+
+    for _ in 0..10 {
+        //mainから各スレッドへのチャンネル
+        let (snd_tx, snd_rx) = mpsc::channel();
+        //各スレッドからmainへのチャンネル
+        let (rcv_tx, rcv_rx) = mpsc::channel();
+        snd_channels.push(snd_tx);
+        rcv_channels.push(rcv_rx);
+
+        handles.push(thread::spawn(move || {
+            let mut data = snd_rx.recv().unwrap();
+            data += 1;
+            let _ = rcv_tx.send(data);
+        }));
+    }
+    //各スレッドにdataの値を送信
+    for x in 0..10 {
+        let _ = snd_channels[x].send(data[x]);
+    }
+    //各スレッドからの結果をdataに格納
+    for x in 0 ..10 {
+        data[x] = rcv_channels[x].recv().unwrap();
+    }
+    for handle in handles { 
+        let _ = handle.join();
+    }
+    dbg!(data);
+
+    use futures:: {executor, future::join_all};
+    use std::future::Future;
+    use std::pin::Pin;
+    use std::task::{Context, Poll};
+
+    struct CountDown(u32);
+    impl Future for CountDown {
+        type Output = String;
+        fn poll(mut self: Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> Poll<String> {
+            if self.0 == 0 {
+                Poll::Ready("Zero!!".to_string())
+            } else {
+                println!("{}", self.0);
+                self.0 -= 1;
+                cx.waker().wake_by_ref();
+                Poll::Pending
+            }
+        }
+    }
+    let countdown_future1 = CountDown(10);
+    let countdown_future2 = CountDown(20);
+    let cd_set = join_all(vec![countdown_future1, countdown_future2]);
+    let res = executor::block_on(cd_set);
+    for(i, s) in res.iter().enumerate() {
+        println!("{}:{}", i, s);
+    }
+    async fn async_add(left:i32, right:i32) -> i32 {
+        left + right
+    }
+    async fn something_great_async_function() -> i32 {
+        let ans = async_add(2,3).await; //この時点で５というあたいを取り出せる。
+        //何か処理を挟むことも出来る。
+        println!("{}",ans);
+        ans
+    }
+    fn some_great_function() -> impl Future<Output = i32> {
+        //let value : i32 = 5;
+        async {
+            let value : i32 = 5;
+            send_to_another_thread_with_borrowing(&value).await
+        }
+        
+    }
+
+    async fn send_to_another_thread_with_borrowing(x : &i32) -> i32 {
+        //何か別スレッドへ送る処理が書かれている想定
+        4
+    }
+    executor::block_on(something_great_async_function());
+
+    //tokioはRustをずっと支えてきた非同期ライブラリらしい。
+    //今も多くのライブラリがtokioに依存している。
+    //macrosフィーチャーを追加し、#[tokio::main]というアトリビュートを利用可能に。
+    //これにより、main関数をasync化できる
+    /*
+    async fn add(left : i32, right: i32) -> i32 {
+        left + right
+    }
+    #[tokio::main]
+    async fn main() {
+        let ans = add(2,3).await;
+        println!("{}",ans)
+    }
+    */
+
+    //async-std は近年登場してきたクレート。tokioは内部実装やAPIがやや複雑なので、それに対処するために
+    //作られたが、まだ歴史は浅い。
+    //attributes フィーチャーを追加することにより、tokioと同じように、async main化出来る。
+    /*
+    #[async_std::main]
+    async fn main () {
+        let ans = async_add(2,3).await;
+        println!("{}", ans);
+    }
+    */
+
+    /* async-trait */
+    // Rustでは、現状トレイトの関数にasyncをつけることはできない。
+    /*
+    trait AsyncTrait {
+        async fn f() {
+            println!("Coundn't compile"); // コンパイルエラー
+        }
+    }
+    */
+    //トレイトに#[async_trait]というアトリビュートを追加すると、async fn ... という関数宣言を出来るようになる。
+    use async_trait::async_trait;
+    #[async_trait]
+    trait AsyncTrait {
+        async fn f() {
+            println!("Counld compile");
+        }
+    }
+
+    //実装を委ねることも出来るよ
+    #[async_trait]
+    pub trait AsyncTrait2 {
+        async fn f(&self);
+    }
+    struct Runner{}
+    #[async_trait]
+    impl AsyncTrait2 for Runner {
+        async fn f(&self) {
+            println!("Hello, async-trait");
+        }
+    }
+    //次は、クレートとモジュールの勉強に入るので、プロジェクトを切り替える。ここまで。
 }
 
 enum Emotion {
